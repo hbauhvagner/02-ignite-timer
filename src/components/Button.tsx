f5eb47e69425a9b3094d269b1fdbc553895038b8
hbauhvagner <hbauhvagner@gmail.com>
2023-03-14 00:31:41 -0300

    Páginas e rotas - React Router DOM

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVarinat } from './Button.styles'
-
-interface ButtonProps {
-  variant?: ButtonVarinat
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
